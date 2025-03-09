d584340ae645253e2a4c98469ea61ef0a8808fe1
Dillon Broaders <113981110+dillon-broaders@users.noreply.github.com>
2025-03-09 22:41:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from keras.callbacks import EarlyStopping
 np.set_printoptions(precision = 3)
 print("TensorFlow version:", tf.__version__)
 
-#reads coordinate file and returns list of molecular coordinates
+# function reads coordinate file and returns list of molecular coordinates
 def read_coordinates(file_name):
     coords, c = [], []
     with open(file_name) as f:
@@ -23,7 +23,7 @@ def read_coordinates(file_name):
                     c = []
     return coords
 
-#reads the energy file and returns an array of molecular energies (i.e., the labels)
+# function reads energy file and returns an array of molecular energies (labels)
 def read_energy(file_name):
     energy = []
     with open(file_name) as f:
@@ -33,7 +33,7 @@ def read_energy(file_name):
     energy_arr = np.array(energy, dtype=float)
     return energy_arr
 
-#calculating the interatmic distance for each molecule (i.e., the features)
+# function calculates the interatmic distance for each molecule (i.e. features)
 def calculate_distance(coordinates):
     distances = np.zeros(shape=(len(coordinates), 3))
     for i, coords in enumerate(coordinates):
@@ -43,7 +43,7 @@ def calculate_distance(coordinates):
         distances[i] = [oh1, oh2, hh]
     return distances
 
-#model architecture
+# model architecture
 def model(node1, node2, node3, reg):
     model = keras.models.Sequential([
     keras.layers.Input(shape = (3,)),
@@ -56,7 +56,7 @@ def model(node1, node2, node3, reg):
     keras.layers.Dense(1, kernel_regularizer = keras.regularizers.l2(reg), use_bias=False)])
     return model
 
-#determine learning curve
+# learning curve
 def learning_curve(model, x_train, y_train, x_test, y_test, training_size, epochs, n_avg):
     train_errors, val_errors = [], []
     print("generating learning curve")
@@ -78,7 +78,7 @@ def learning_curve(model, x_train, y_train, x_test, y_test, training_size, epoch
         val_errors.append(val_avg)
     return np.sqrt(train_errors), np.sqrt(val_errors)
 
-#plotting predicitions
+# function returns plot of energy predicitions
 def plot_predictions(y_test, predictions, ax, file_name):
     ax.scatter(y_test, predictions, color = "blue", alpha = 0.7)
     ax.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], ls = "--", c = "black")
@@ -87,7 +87,7 @@ def plot_predictions(y_test, predictions, ax, file_name):
     ax.set_ylabel("Predicted energy values")
     ax.grid()
 
-#plotting loss vs epochs
+# function returns plot of loss vs epochs
 def plot_loss(training_loss, validation_loss, ax, file_name):
     ax.plot(training_loss, label = "Training loss", c = "blue")
     ax.plot(validation_loss, label = "Validation loss", c = "orange")
@@ -97,7 +97,7 @@ def plot_loss(training_loss, validation_loss, ax, file_name):
     ax.grid()
     ax.legend()
 
-#plotting learning curve
+# function returns learning curve
 def plot_learning_curve(training_points, train_errors, val_errors, ax, file_name):
     ax.plot(training_points, train_errors, "-o", label = "Training")
     ax.plot(training_points, val_errors, "-o", label = "Validation")
@@ -108,29 +108,34 @@ def plot_learning_curve(training_points, train_errors, val_errors, ax, file_name
     ax.grid()
 
 if __name__ == "__main__":
-    #looping through both training data sets and generating the three plots for each
+    # loop through both training data sets and generating the three plots for each
     data_sets = [("H2O_unrotated.xyz", "H2O_unrotated.ener"), ("H2O_rotated.xyz", "H2O_rotated.ener")]
     fig, axes = plt.subplots(3, len(data_sets), figsize = (15, 15))
     for i, (coord_file, energy_file) in enumerate(data_sets):
-        #reading the training/test files
+        
+        # read the training/test files
         training_coords = read_coordinates(file_name = coord_file)
         test = read_coordinates(file_name = "H2O_test.xyz")
-        #defining the training and test data 
+        
+        # define the training and test data 
         x_train = calculate_distance(training_coords)
         y_train = read_energy(file_name = energy_file)
         x_test = calculate_distance(test)
         y_test = read_energy(file_name = "H2O_test.ener")
-        #scale the features 
+        
+        # scale features 
         scalar_x = StandardScaler()
         scalar_x.fit(x_train)
         x_train_scaled = scalar_x.transform(x_train)
         x_test_scaled = scalar_x.transform(x_test)
-        #scale the labels
+        
+        # scale labels
         scalar_y = StandardScaler()
         scalar_y.fit(y_train.reshape(-1, 1))
         y_train_scaled = scalar_y.transform(y_train.reshape(-1, 1))
         y_test_scaled = scalar_y.transform(y_test.reshape(-1, 1))
-        #compile the model
+        
+        # compile model
         epochs = 500
         early_stopping = EarlyStopping(monitor = "val_loss", patience = 10, 
                                     restore_best_weights = True)
@@ -138,33 +143,30 @@ if __name__ == "__main__":
         model_.compile(optimizer = keras.optimizers.Adam(learning_rate = 0.002),
                     loss = "mse",
                     metrics = ["mse"])
-        #fit model
+        
+        # fit model
         history = model_.fit(x_train_scaled, y_train_scaled, 
                             epochs = epochs, 
                             batch_size = 32, 
                         validation_split = 0.20,
                         callbacks=[early_stopping])
-        #calculate loss values
+        
+        # calculate loss values
         training_loss = history.history["loss"]
         validation_loss = history.history["val_loss"]
-        #get predicitions
+        
+        # get predicitions
         predictions = model_.predict(x_test_scaled)
         y_val = scalar_y.inverse_transform(predictions)
-        #generating the learning curve (RMSE as function of training set size)
+        
+        # generate learning curve (RMSE as function of training set size)
         training_size = [50, 100, 250, 500, 750, 1000, 1500, 1749]
         train_errors, val_errors = learning_curve(model_, x_train_scaled, y_train_scaled, 
                                     x_test_scaled, y_test_scaled, training_size, epochs = epochs, n_avg = 10)
-        #plotting curves
+        
         plot_predictions(y_test, y_val,axes[0, i], energy_file)
         plot_loss(training_loss, validation_loss,axes[1, i], energy_file )
         plot_learning_curve(training_size, train_errors, val_errors, axes[2, i], energy_file)
     plt.tight_layout()
     plt.savefig("subplots.png", dpi = 400)
     plt.show()
-
-
-
-
-
-
-
